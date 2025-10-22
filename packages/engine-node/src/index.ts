d7aa5d2a835769e1e01060949c2251e156e94ad3
Oleg Smirnov <sovaz1997@yandex.ru>
2025-10-23 00:00:07 +0600

    initialization

---
+++ packages/engine-node/src/index.ts
@@ -0,0 +1,7 @@
+export function createEngine() {
+  // Placeholder: wire up the core engine implementation.
+  console.log('lol')
+  throw new Error('createEngine is not implemented yet');
+}
+
+createEngine()
