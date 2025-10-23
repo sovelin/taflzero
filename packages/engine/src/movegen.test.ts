926714166fe057e116953302b72e14e452a3554b
Oleg Smirnov <sovaz1997@yandex.ru>
2025-10-24 00:42:28 +0600

    add some tests

--- packages/engine/src/movegen.test.ts
+++ packages/engine/src/movegen.test.ts
@@ -1,11 +1,37 @@
-import {clearBoard, createBoard} from "./board";
+import {clearBoard, createBoard, setPiece} from "./board";
 import {createMoveGenerator} from "./movegen";
+import {Piece, Side} from "./types";
+import {getSquareFromAlgebraic} from "./utils";
+import {printBoard} from "./print";
+import {initLineMoves} from "./attackers";
+import {beforeAll, describe, expect, it} from "vitest";
 
 describe('Move Generation Tests', () => {
-  it('should generate correct moves for a given board position', () => {
+  beforeAll(() => {
+    console.log('lol')
+    initLineMoves()
+  })
+
+  it('clear board: no moves', () => {
     const generator = createMoveGenerator();
     const board = createBoard();
     clearBoard(board);
+    generator.movegen(board);
     expect(generator.movesCount).toBe(0);
+  })
+
+  it('2 pieces on the board', () => {
+    const generator = createMoveGenerator();
+    const board = createBoard();
+
+
+
+    clearBoard(board);
+    board.sideToMove = Side.ATTACKERS;
+    setPiece(board, getSquareFromAlgebraic("a1"), Piece.ATTACKER);
+    generator.movegen(board);
+    printBoard(board)
+    console.log(`Generated ${generator.movesCount} moves`);
+    //expect(generator.movesCount).toBe(7);
   })
 })
