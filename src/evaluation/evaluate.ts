4e479a273e518206a6ef8a2c09f5992ddb406c53
Oleg Smirnov <sovaz1997@yandex.ru>
2025-11-04 02:08:47 +0300

    PST eval (#11)

--- src/evaluation/evaluate.ts
+++ src/evaluation/evaluate.ts
@@ -1,4 +1,5 @@
-import {Board, Side} from "@/board";
+import {Board, Piece, Side} from "@/board";
+import {PieceWeights, PSQT_ATK, PSQT_DEF, PSQT_KING} from "@/evaluation/constants";
 
 export const sidedEval = (board: Board, score: number): number => {
   return board.sideToMove === Side.DEFENDERS
@@ -9,5 +10,22 @@ export const sidedEval = (board: Board, score: number): number => {
 export const evaluateBoard = (board: Board): number => {
   // Simple evaluation function: difference in number of pieces
   const {attackersCount, defendersCount} = board;
-  return sidedEval(board, defendersCount - attackersCount / 2);
+
+  let score = 0
+  score += defendersCount * PieceWeights[Piece.DEFENDER];
+  score -= attackersCount * PieceWeights[Piece.ATTACKER];
+
+  score += PSQT_KING[board.kingSq];
+
+  for (let i = 0; i < defendersCount; i++) {
+    const sq = board.defenders[i];
+    score += PSQT_DEF[sq];
+  }
+
+  for (let i = 0; i < attackersCount; i++) {
+    const sq = board.attackers[i];
+    score -= PSQT_ATK[sq];
+  }
+
+  return sidedEval(board, score);
 }
