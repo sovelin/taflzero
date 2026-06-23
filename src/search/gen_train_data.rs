0a96bae13f6de9edd0799d6059544148ca531310
sovaz1997 <sovaz19971997@gmail.com>
2026-06-23 21:41:43 +0300

    update train data gen

--- src/search/gen_train_data.rs
+++ src/search/gen_train_data.rs
@@ -120,10 +120,20 @@ fn play_game(
                 no_capture_counter += 1;
             }
 
-            if no_capture_counter >= 100 {
+            if no_capture_counter >= 500 || move_number >= 700 {
                 // end the game as a draw
                 game_result = None;
-                terminal_str = Some("draw_nocapture");
+                terminal_str = Some(if no_capture_counter >= 500 {
+                    "draw_nocapture"
+                } else {
+                    "draw_limit"
+                });
+                break;
+            }
+
+            // Treat threefold repetition as draw for training
+            if is_threefold_repetition(&board) {
+                game_result = None;
                 break;
             }
 
@@ -199,7 +209,6 @@ pub fn gen_train_data(
 
         let (res, game_result, terminal_str) = play_game(nn, &mut search_data, variant);
 
-        positions_generated += res.len();
         games_saved += 1;
         match game_result {
             Some(Side::ATTACKERS) => attacker_wins_saved += 1,
@@ -242,8 +251,11 @@ pub fn gen_train_data(
             log_writer.flush().expect("Cannot flush gamelog");
         }
 
-        for sample in res {
-            sample.write_to(&mut writer).expect("Cannot write sample");
+        if game_result.is_some() {
+            positions_generated += res.len();
+            for sample in res {
+                sample.write_to(&mut writer).expect("Cannot write sample");
+            }
         }
     }
 }
