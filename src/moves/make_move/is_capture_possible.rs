eb28741babddb957164b839118064fd3fce19690
sovaz1997 <sovaz19971997@gmail.com>
2026-06-23 21:43:43 +0300

    fmt

--- src/moves/make_move/is_capture_possible.rs
+++ src/moves/make_move/is_capture_possible.rs
@@ -40,7 +40,9 @@ pub fn is_capture_possible(
     enemy_sq_1: Square,
     enemy_sq_2: Square,
 ) -> bool {
-    if board.rules.rules().is_king_strong && board.board[target_sq] == Piece::KING || board.board[target_sq] == Piece::EMPTY {
+    if board.rules.rules().is_king_strong && board.board[target_sq] == Piece::KING
+        || board.board[target_sq] == Piece::EMPTY
+    {
         return false;
     }
 
